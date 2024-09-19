6add52572af6a5cb75d4207d9245e3157d8a9927
Shebin Koshy <shebin.koshy@thoughtworks.com>
2024-09-19 19:33:38 +0530

    android version condition check compiletime issue resolved

--- android/src/main/java/com/akurupela/oneroot/core/OneRoot.java
+++ android/src/main/java/com/akurupela/oneroot/core/OneRoot.java
@@ -242,24 +242,19 @@ public class OneRoot {
     }
 
     public boolean checkForMagiskNative() {
-        return false;
-        // try {
-        //     if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        //         if (!canLoadNativeLibrary()){
-        //             return false;
-        //         }
-        //         Phaser phaser = new Phaser();
-        //         try {
-        //             phaser.setLogDebugMessages(loggingEnabled);
-        //             return phaser.checkForMagiskUDS() > 0;
-        //         } catch (UnsatisfiedLinkError e) {
-        //             return false;
-        //         }
-        //     }else{
-        //         return false;
-        //     }
-        // } catch (Throwable e) {
-        //     return false;
-        // }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                if (!canLoadNativeLibrary()){
+                    return false;
+                }
+                Phaser phaser = new Phaser();
+                try {
+                    phaser.setLogDebugMessages(loggingEnabled);
+                    return phaser.checkForMagiskUDS() > 0;
+                } catch (UnsatisfiedLinkError e) {
+                    return false;
+                }
+        } else {
+            return false;
+        }
     }
 }
