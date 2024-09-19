31997bbaacf0f3fcf40567c64a3cda626b2896e7
shebin-tw <65023866+shebin-tw@users.noreply.github.com>
2024-09-19 10:46:45 +0530

    Update OneRoot.java

--- android/src/main/java/com/akurupela/oneroot/core/OneRoot.java
+++ android/src/main/java/com/akurupela/oneroot/core/OneRoot.java
@@ -242,18 +242,22 @@ public class OneRoot {
     }
 
     public boolean checkForMagiskNative() {
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (!canLoadNativeLibrary()){
+        try {
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                if (!canLoadNativeLibrary()){
+                    return false;
+                }
+                Phaser phaser = new Phaser();
+                try {
+                    phaser.setLogDebugMessages(loggingEnabled);
+                    return phaser.checkForMagiskUDS() > 0;
+                } catch (UnsatisfiedLinkError e) {
+                    return false;
+                }
+            }else{
                 return false;
             }
-            Phaser phaser = new Phaser();
-            try {
-                phaser.setLogDebugMessages(loggingEnabled);
-                return phaser.checkForMagiskUDS() > 0;
-            } catch (UnsatisfiedLinkError e) {
-                return false;
-            }
-        }else{
+        } catch (Throwable e) {
             return false;
         }
     }
